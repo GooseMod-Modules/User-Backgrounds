e49ac4153af3e7dcae0f5a173b9c5d0e3ff69253
Oj <oj@oojmed.com>
2022-05-01 22:46:28 +0100

    Custom usrbg banner tag, fix if user has no banner

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import Plugin from '@goosemod/plugin';
 import { patch } from '@goosemod/patcher';
 import * as Webpack from '@goosemod/webpack';
+import { React } from '@goosemod/webpack/common';
 
 const UserPopoutContainer = Webpack.find(x => x?.type?.displayName === 'UserPopoutContainer'); // Memo, so within .type, not .default
 const UserBanner = Webpack.find(x => x.default?.displayName === 'UserBanner')
+const { TextBadge } = goosemod.webpackModules.findByProps('TextBadge');
+
 
 let database;
 (async () => { // Fetch and load DB async
@@ -14,12 +17,24 @@ let database;
 class Usrbg extends Plugin {
   onImport() {
     this.enqueueUnpatch(patch(UserPopoutContainer, 'type', (_args, res) => { // Patch UserPopoutContainer with fake user.banner so it uses "premium" layout for banner
-      res.props.user.banner = 'null'; // It loads a formatted CDN url (e.g. example.com/banners/user_id/banner_id) so we can't just put URL here
+      const user = res.props.user;
+      if (database[user.id]) res.props.user.banner = 'null'; // It loads a formatted CDN url (e.g. example.com/banners/user_id/banner_id) so we can't just put URL here
+
       return res;
     }));
 
     this.enqueueUnpatch(patch(UserBanner, 'default', ([ { user } ], res) => { // Patch UserBanner so it actually shows banner
-      res.props.style.backgroundImage = 'url("' + database[user.id] + '")';
+      if (database[user.id]) {
+        res.props.style.backgroundImage = 'url("' + database[user.id] + '")';
+
+        res.props.children[0] = React.createElement(TextBadge, {
+          color: 'rgba(32, 34, 37, 0.8)',
+          className: 'premiumIconWrapper-32h0Ri',
+          text: React.createElement('span', { }, 'USRBG')
+        });
+      }
+
+      return res;
     }));
   }
 }
