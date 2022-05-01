e9780ca1e4060302ef84f08eb27b193b3066b8f3
Oj <oj@oojmed.com>
2022-05-01 14:22:57 +0100

    Add comments explaining process

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Plugin from '@goosemod/plugin';
 import { patch } from '@goosemod/patcher';
 import * as Webpack from '@goosemod/webpack';
 
-const UserPopoutContainer = Webpack.find(x => x?.type?.displayName === 'UserPopoutContainer');
+const UserPopoutContainer = Webpack.find(x => x?.type?.displayName === 'UserPopoutContainer'); // Memo, so within .type, not .default
 const UserBanner = Webpack.find(x => x.default?.displayName === 'UserBanner')
 
 let database;
@@ -13,12 +13,12 @@ let database;
 
 class Usrbg extends Plugin {
   onImport() {
-    this.enqueueUnpatch(patch(UserPopoutContainer, 'type', (_args, res) => {
-      res.props.user.banner = 'null'; // ;)
+    this.enqueueUnpatch(patch(UserPopoutContainer, 'type', (_args, res) => { // Patch UserPopoutContainer with fake user.banner so it uses "premium" layout for banner
+      res.props.user.banner = 'null'; // It loads a formatted CDN url (e.g. example.com/banners/user_id/banner_id) so we can't just put URL here
       return res;
     }));
 
-    this.enqueueUnpatch(patch(UserBanner, 'default', ([ { user } ], res) => {
+    this.enqueueUnpatch(patch(UserBanner, 'default', ([ { user } ], res) => { // Patch UserBanner so it actually shows banner
       res.props.style.backgroundImage = 'url("' + database[user.id] + '")';
     }));
   }
